e83379acaff1ca421b025e318c4bf4320e47e270
Imad <mk.imaduddin@outlook.com>
2016-01-31 09:07:31 +0000

    Add API url-shortener

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,7 @@
 
 var path = process.cwd();
 var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
+var MongoClient = require('mongodb').MongoClient;
 
 module.exports = function (app, passport) {
 
@@ -54,4 +55,96 @@ module.exports = function (app, passport) {
 		.get(isLoggedIn, clickHandler.getClicks)
 		.post(isLoggedIn, clickHandler.addClick)
 		.delete(isLoggedIn, clickHandler.resetClicks);
+		
+	app.route(/\/new\/.+/)
+		.get( function (req, res) {
+			var match = req.path.match(/^\/new\/(.+)/);
+			var url = match[1];
+			var isValid = /^\D.+\:/.test(url);
+			
+			console.log("=========")
+			console.log(url);
+			console.log('---------')
+			
+			var dburl = process.env.MONGO_URI;
+			
+			if (isValid) {
+				var length, shortUrl;
+				console.log('valid');
+				MongoClient.connect(dburl, function (err, db) {
+					if (err) throw err;
+					
+					var collection = db.collection('shorts');
+					
+					collection.find({url:url}).toArray(function (err, item) {
+						if (err) throw err;
+						
+						var isExist = (item.length == 0) ? false : true;
+						
+						if (!isExist) {
+							collection.count(function (err, count) {
+								console.log('not exist');
+								length = count+1;
+								
+								collection.insertOne(
+									{
+										url: url,
+										short: length
+									}, function (err, result) {
+										
+										var jsonSent = {
+											original_url: url,
+											short_url: req.hostname + "/" + length
+										};
+										res.json(jsonSent);
+									}
+								);
+							}); // end count
+						} else {
+							console.log('exist');
+							var jsonSent = {
+								original_url: item[0].url,
+								short_url: req.hostname + "/" + item[0].short
+							};
+							res.json(jsonSent);
+						}
+					}); // end find
+				}); // end connect
+				
+			} else {
+				console.log('not valid');
+				var jsonSent = {
+					error: "url is not valid"
+				}
+				res.json(jsonSent);
+			} // end valid
+		}); // end route new
+		
+	app.route('/:short')
+		.get(function (req, res) {
+
+			var dburl = process.env.MONGO_URI;
+			MongoClient.connect(dburl, function (err, db) {
+				if (err) throw err;
+					
+				var collection = db.collection('shorts');
+				var short = parseInt(req.path.match(/\d+/)[0]);
+				
+				collection.find({short:short}).toArray(function (err, item) {
+					if (err) throw err;
+
+					var isExist = (item.length == 0) ? false : true;
+					
+					if (isExist) {
+						console.log('exist');
+						var url = item[0].url;
+						res.redirect(url);
+					} else {
+						console.log('not exist');
+						res.end();
+					}
+				});
+			
+			});
+		});
 };
